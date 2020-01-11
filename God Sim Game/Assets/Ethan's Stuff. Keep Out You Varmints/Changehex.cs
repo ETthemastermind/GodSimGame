526f2c57a9a2a8c47c86e57cf738b6dc34142cbd
s4911672 <s4911672@bournemouth.ac.uk>
2020-01-11 22:38:04 +0000

    Pushing LM assets cuz unity no like tom

---
+++ God Sim Game/Assets/Ethan's Stuff. Keep Out You Varmints/Changehex.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changehex : MonoBehaviour
+{
+
+    public GameObject DestBiome;
+    private Vector3 DestPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Changing Biome");
+            DestPosition = gameObject.transform.position;
+            Instantiate(DestBiome, new Vector3(DestPosition.x, DestPosition.y, DestPosition.z), Quaternion.identity);
+
+
+
+
+        }
+    }
+}
