b2c1bc063e0af36f149c894d1252ccc26164d1bf
TomUdaleVFX <tomudalevfx@gmail.com>
2020-01-13 23:52:56 +0000

    First commit from Tom, hope it isn't fucked

---
+++ God Sim Game/Assets/Tom's Stuff/GestureDetectionScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureDetectionScript : MonoBehaviour
+{
+    public void OnGestureActivate()
+    {
+        Debug.Log("HandClosed");
+    }
+
+    public void OnGestureDeactivate()
+    {
+        Debug.Log("HandOpen");
+    }
+}
---
+++ God Sim Game/Assets/Tom's Stuff/PointAtObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtObject : MonoBehaviour
+{
+    public GameObject Finger;
+    public LayerMask layerMask;
+    float currentHitDistance;
+    float maxDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(Finger.transform.position, 3.0f, Finger.transform.forward, out hit, 10.0f, layerMask, QueryTriggerInteraction.UseGlobal))
+        {
+            currentHitDistance = hit.distance;
+            //Debug.Log("We hit a " + hit.collider.name);
+        }
+        else
+        {
+            currentHitDistance = 30.0f;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Debug.DrawLine(Finger.transform.position, Finger.transform.position + Finger.transform.forward * currentHitDistance);
+        Gizmos.DrawWireSphere(Finger.transform.position + Finger.transform.forward * currentHitDistance, 2.5f);
+    }
+}
